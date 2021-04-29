a3545c318005425e4adaf6a98c66da34160b8fda
Alessio Olivieri <Alessio.Olivieri@zodiakactive.com>
2021-04-29 16:30:46 +0200

    1st commit

---
+++ src/main/java/it/olly/stereogram/StereogramGenerator.java
@@ -0,0 +1,109 @@
+package it.olly.stereogram;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class StereogramGenerator {
+    static int patternHeight = 200;
+    static int patternWidth = 142;
+
+    static boolean[][] pattern = new boolean[patternHeight][patternWidth];
+
+    static boolean[][] getDisparityMap() throws Exception {
+        int w = 600;
+        int h = 400;
+        boolean[][] ret = new boolean[h][w];
+
+        // todo read from image
+        BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.setColor(Color.black);
+        g2d.fillRect(0, 0, w, h);
+        g2d.setColor(Color.white);
+        g2d.fillRect(150, 100, 50, 50);
+        g2d.fillRect(350, 200, 100, 35);
+        g2d.dispose();
+
+        // Save as PNG
+        File file = new File("/temp/disparity.png");
+        ImageIO.write(bufferedImage, "png", file);
+
+        // compose disparity map. TODO -> integer with a level? 1-5?
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                // Getting pixel color by position x and y
+                int clr = bufferedImage.getRGB(x, y);
+                int red = (clr & 0x00ff0000) >> 16;
+                int green = (clr & 0x0000ff00) >> 8;
+                int blue = clr & 0x000000ff;
+                ret[y][x] = red > 0 || green > 0 || blue > 0;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // generate binary rnd pattern black/white
+        for (int y = 0; y < patternHeight; y++) {
+            for (int x = 0; x < patternWidth; x++) {
+                pattern[y][x] = Math.random() < 0.5;
+            }
+        }
+
+        // get the disparity map. TODO load a gray-scale image
+        boolean[][] disparityMap = getDisparityMap();
+
+        // Constructs a BufferedImage of one of the predefined image types.
+        BufferedImage bufferedImage = new BufferedImage(disparityMap[0].length, disparityMap.length, BufferedImage.TYPE_INT_RGB);
+
+        // Create a graphics which can be used to draw into the buffered image
+        Graphics2D g2d = bufferedImage.createGraphics();
+
+        // write pattern n times to image
+        for (int x = 0; x < bufferedImage.getWidth(); x++) {
+            for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                // write pattern pixel
+                int xOnPattern = x % patternWidth;
+                int yOnPattern = y % patternHeight;
+                if (pattern[yOnPattern][xOnPattern]) {
+                    g2d.setColor(Color.white);
+                } else {
+                    g2d.setColor(Color.black);
+                }
+                g2d.fillRect(x, y, 1, 1);
+            }
+        }
+
+        // shift pixels if needed
+        for (int x = 0; x < bufferedImage.getWidth(); x++) {
+            for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                if (disparityMap[y][x]) {
+                    // write pattern pixel
+                    int xOnPattern = x % patternWidth;
+                    int yOnPattern = y % patternHeight;
+                    if (pattern[yOnPattern][xOnPattern]) {
+                        g2d.setColor(Color.white);
+                    } else {
+                        g2d.setColor(Color.black);
+                    }
+
+                    int shift = 2;
+                    // check boundaries (if x+shift>=width, do nothing)
+                    g2d.fillRect(x + shift, y, 1, 1);
+                }
+            }
+        }
+
+        // Disposes of this graphics context and releases any system resources that it is using.
+        g2d.dispose();
+
+        // Save as PNG
+        File file = new File("/temp/myimage.png");
+        ImageIO.write(bufferedImage, "png", file);
+    }
+
+}
---
+++ src/main/java/it/olly/stereogram/StereogramGenerator2.java
@@ -0,0 +1,122 @@
+package it.olly.stereogram;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class StereogramGenerator2 {
+    static int patternWidth = 142;
+
+    static int[][] getDisparityMap() throws Exception {
+        int w = 500;
+        int h = 150;
+        int[][] ret = new int[w][h];
+
+        // todo read from image
+        BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.setColor(Color.black);
+        g2d.fillRect(0, 0, w, h);
+        g2d.setColor(Color.white);
+        g2d.fillRect(50, 50, 50, 50);
+        g2d.fillOval(175, 25, 100, 100);
+        g2d.fillRect(350, 100, 100, 35);
+        g2d.dispose();
+
+        // Save as PNG
+        File file = new File("/temp/disparity.png");
+        ImageIO.write(bufferedImage, "png", file);
+
+        // compose disparity map. TODO -> integer with a level? 1-5?
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                // Getting pixel color by position x and y
+                int clr = bufferedImage.getRGB(x, y);
+                int red = (clr & 0x00ff0000) >> 16;
+                int green = (clr & 0x0000ff00) >> 8;
+                int blue = clr & 0x000000ff;
+                ret[x][y] = (red > 0 || green > 0 || blue > 0) ? 1 : 0;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // disparityMap and pattern array has to be considered with [col][row] (like x,y)
+
+        // get the disparity map. TODO load a gray-scale image
+        int[][] disparityMap = getDisparityMap();
+        int disparityWidth = disparityMap.length;
+        int disparityHeight = disparityMap[0].length;
+
+        int totSteps = 1 + (disparityWidth / patternWidth);
+
+        int[][] pattern = new int[patternWidth][disparityHeight];
+        // generate binary rnd pattern black/white
+        for (int x = 0; x < patternWidth; x++) {
+            for (int y = 0; y < disparityHeight; y++) {
+                pattern[x][y] = (Math.random() < 0.5) ? 1 : 0;
+            }
+        }
+
+        // Constructs a BufferedImage of one of the predefined image types.
+        // the image created will have size = (n*pattern.width, disparitymap.height)
+        BufferedImage bufferedImage = new BufferedImage(patternWidth
+                * (totSteps + 1), disparityHeight, BufferedImage.TYPE_INT_RGB);
+
+        // Create a graphics which can be used to draw into the buffered image
+        Graphics2D g2d = bufferedImage.createGraphics();
+
+        // write pattern for the 1st column (with width = pattern's width) that will be the base for the next steps
+        for (int x = 0; x < patternWidth; x++) {
+            for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                // write pattern pixel
+                if (pattern[x][y] > 0) {
+                    g2d.setColor(Color.white);
+                } else {
+                    g2d.setColor(Color.black);
+                }
+                g2d.fillRect(x, y, 1, 1);
+            }
+        }
+
+        // starts the process. every step will be the size of patternWidth
+        for (int step = 1; step <= totSteps; step++) {
+            // write the previous pattern as my step's background
+            for (int x = 0; x < patternWidth; x++) {
+                for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                    int rgb = bufferedImage.getRGB(realX(x, step - 1), y);
+                    g2d.setColor(new Color(rgb));
+                    g2d.fillRect(realX(x, step), y, 1, 1);
+                }
+            }
+
+            // write shifted things
+            for (int x = 0; x < patternWidth; x++) {
+                for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                    int xOnDisparityMap = realX(x, step) - patternWidth;
+                    if (xOnDisparityMap < disparityWidth && disparityMap[xOnDisparityMap][y] > 0) {
+                        int rgb = bufferedImage.getRGB(realX(x, step), y);
+                        // TODO check boundaries
+                        g2d.setColor(new Color(rgb));
+                        g2d.fillRect(realX(x, step) - 2, y, 1, 1);
+                    }
+                }
+            }
+        }
+
+        // Disposes of this graphics context and releases any system resources that it is using.
+        g2d.dispose();
+
+        // Save as PNG
+        File file = new File("/temp/myimage.png");
+        ImageIO.write(bufferedImage, "png", file);
+    }
+
+    private static int realX(int x, int step) {
+        return step * patternWidth + x;
+    }
+}
